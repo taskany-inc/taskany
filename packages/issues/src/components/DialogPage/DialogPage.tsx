cb1d3896cc03c6ed75aa244c30d14de707c673c6
Anton Vinogradov <winogradovaa@gmail.com>
2021-05-04 22:33:20 +0300

    feat(issues): support tab title in DialogPage

--- packages/issues/src/components/DialogPage/DialogPage.tsx
+++ packages/issues/src/components/DialogPage/DialogPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
 
 import { renderIf } from '../../utils/conditianalRender';
 
 interface DialogPageProps {
+    title?: string;
     header?: React.ReactNode;
     content: React.ReactNode;
     footer?: React.ReactNode;
@@ -19,15 +21,24 @@ const StyledDialogPageContent = styled.div``;
 const StyledDialogPageFooter = styled.div``;
 
 export const DialogPage: React.FC<DialogPageProps> = (props) => {
+    const title = renderIf(
+        props.title,
+        <Head>
+            <title>{props.title}</title>
+        </Head>,
+    );
     const header = renderIf(props.header, <StyledDialogPageHeader>{props.header}</StyledDialogPageHeader>);
     const content = <StyledDialogPageContent>{props.content}</StyledDialogPageContent>;
     const footer = renderIf(props.footer, <StyledDialogPageFooter>{props.footer}</StyledDialogPageFooter>);
 
     return (
-        <StyledDialogPage>
-            {header}
-            {content}
-            {footer}
-        </StyledDialogPage>
+        <>
+            {title}
+            <StyledDialogPage>
+                {header}
+                {content}
+                {footer}
+            </StyledDialogPage>
+        </>
     );
 };
