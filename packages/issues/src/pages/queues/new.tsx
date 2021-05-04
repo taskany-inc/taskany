13d0aa8571e880a193208062d4152263eb092b3c
Anton Vinogradov <winogradovaa@gmail.com>
2021-05-04 22:36:15 +0300

    feat(issues): use form on the new queue page

--- packages/issues/src/pages/queues/new.tsx
+++ packages/issues/src/pages/queues/new.tsx
@@ -2,8 +2,52 @@ import { useEffect } from 'react';
 import { useSession, getSession } from 'next-auth/client';
 
 import { DialogPage } from '../../components/DialogPage/DialogPage';
+import { Form, useFormState } from '../../components/Form/Form';
+import { ButtonSubmit } from '../../components/Button/_submit/Button_submit';
 
 // import { useQuery } from '../@generated/queries';
+
+const PageHeader: React.FC = () => (
+    <>
+        <div>Tell us about your queue</div>
+        <h1>Setup your new queue</h1>
+    </>
+);
+
+const PageContent: React.FC = () => {
+    const state = useFormState({
+        fields: {
+            name: {
+                type: 'input',
+                required: true,
+                label: 'Queue name',
+                placeholder: 'ololol',
+            },
+            description: {
+                type: 'input',
+                label: 'Description',
+            },
+        },
+        onValidate(fields) {
+            const errors: Partial<Record<keyof typeof fields, string>> = {};
+            if (fields.name !== 'ccc') {
+                errors.name = 'Error!';
+                throw errors;
+            }
+        },
+        onSubmit(fields) {
+            console.log('submit!', fields);
+        },
+    });
+
+    return (
+        <>
+            <Form {...state}>
+                <ButtonSubmit {...state} text="Create" view="primary" />
+            </Form>
+        </>
+    );
+};
 
 export default function Page() {
     const [session, loading] = useSession();
@@ -19,17 +63,7 @@ export default function Page() {
         return <h1>Access denied</h1>;
     }
 
-    return (
-        <DialogPage
-            header={
-                <>
-                    <div>Tell us about your queue</div>
-                    <h1>Setup your new queue</h1>
-                </>
-            }
-            content={'form'}
-        />
-    );
+    return <DialogPage title="Create new queue" header={<PageHeader />} content={<PageContent />} />;
 }
 
 export async function getServerSideProps(context) {
