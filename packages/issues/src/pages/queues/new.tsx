520d87e64690e7ae97e930d9038a51d1d636c5bc
Anton Vinogradov <winogradovaa@gmail.com>
2021-05-04 23:18:58 +0300

    feat(issues): better DialogPage API

--- packages/issues/src/pages/queues/new.tsx
+++ packages/issues/src/pages/queues/new.tsx
@@ -1,20 +1,32 @@
 import { useEffect } from 'react';
 import { useSession, getSession } from 'next-auth/client';
 
-import { DialogPage } from '../../components/DialogPage/DialogPage';
+import { H1, Hi } from '../../components/Typo/Typo';
+import {
+    DialogPage,
+    DialogPageTitle,
+    DialogPageHeader,
+    DialogPageContent,
+} from '../../components/DialogPage/DialogPage';
 import { Form, useFormState, FormErrors } from '../../components/Form/Form';
 import { ButtonSubmit } from '../../components/Button/_submit/Button_submit';
 
 // import { useQuery } from '../@generated/queries';
 
-const PageHeader: React.FC = () => (
-    <>
-        <div>Tell us about your queue</div>
-        <h1>Setup your new queue</h1>
-    </>
-);
+export default function Page() {
+    const [session, loading] = useSession();
+    // const { data } = useQuery();
 
-const PageContent: React.FC = () => {
+    useEffect(() => {}, [session]);
+
+    // When rendering client side don't display anything until loading is complete
+    if (typeof window !== 'undefined' && loading) return null;
+
+    // If no session exists, display access denied message
+    if (!session) {
+        return <h1>Access denied</h1>;
+    }
+
     const state = useFormState({
         fields: {
             name: {
@@ -41,27 +53,19 @@ const PageContent: React.FC = () => {
     });
 
     return (
-        <Form {...state}>
-            <ButtonSubmit {...state} text="Create" view="primary" />
-        </Form>
+        <DialogPage>
+            <DialogPageTitle>Create new queue</DialogPageTitle>
+            <DialogPageHeader>
+                <Hi>Tell us about your queue</Hi>
+                <H1>Setup your new queue</H1>
+            </DialogPageHeader>
+            <DialogPageContent>
+                <Form {...state}>
+                    <ButtonSubmit {...state} text="Create" view="primary" />
+                </Form>
+            </DialogPageContent>
+        </DialogPage>
     );
-};
-
-export default function Page() {
-    const [session, loading] = useSession();
-    // const { data } = useQuery();
-
-    useEffect(() => {}, [session]);
-
-    // When rendering client side don't display anything until loading is complete
-    if (typeof window !== 'undefined' && loading) return null;
-
-    // If no session exists, display access denied message
-    if (!session) {
-        return <h1>Access denied</h1>;
-    }
-
-    return <DialogPage title="Create new queue" header={<PageHeader />} content={<PageContent />} />;
 }
 
 export async function getServerSideProps(context) {
