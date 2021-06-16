07e8cb8475c932a2bdff19ed4e968547902a2577
Anton Vinogradov <winogradovaa@gmail.com>
2021-06-17 00:18:08 +0300

    feat(issues): add some icons

--- packages/issues/src/components/Icon/Icon.tsx
+++ packages/issues/src/components/Icon/Icon.tsx
@@ -5,12 +5,22 @@ const TreeIcon = dynamic(() => import('./assets/tree.svg'));
 const BellIcon = dynamic(() => import('./assets/bell.svg'));
 const PlusIcon = dynamic(() => import('./assets/plus.svg'));
 const UserIcon = dynamic(() => import('./assets/user.svg'));
+const BookmarkIcon = dynamic(() => import('./assets/bookmark.svg'));
+const BulbOnIcon = dynamic(() => import('./assets/bulb-on.svg'));
+const CogIcon = dynamic(() => import('./assets/cog.svg'));
+const KanabanIcon = dynamic(() => import('./assets/kanban.svg'));
+const PieChartIcon = dynamic(() => import('./assets/pie-chart.svg'));
 
 const componentsMap = {
     tree: TreeIcon,
     bell: BellIcon,
     plus: PlusIcon,
     user: UserIcon,
+    bookmark: BookmarkIcon,
+    bulbOn: BulbOnIcon,
+    cog: CogIcon,
+    kanban: KanabanIcon,
+    pieChart: PieChartIcon,
 };
 
 const sizesMap = {
@@ -24,14 +34,15 @@ interface IconProps {
     size: keyof typeof sizesMap;
     color?: string;
     stroke?: number;
+    className?: string;
 }
 
-export const Icon: React.FC<IconProps> = ({ type, size, color, stroke = 2 }) => {
+export const Icon: React.FC<IconProps> = ({ type, size, color, stroke = 1, className }) => {
     const Component = componentsMap[type];
     const sizePx = `${sizesMap[size]}px`;
 
     return (
-        <div style={{ display: 'inherit' }}>
+        <div className={className}>
             <Component width={sizePx} height={sizePx} color={color} strokeWidth={stroke} />
         </div>
     );
