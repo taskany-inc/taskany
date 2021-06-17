ef9a027cb3d7369ec0eb3724a2352e9e0c5ddf74
Anton Vinogradov <winogradovaa@gmail.com>
2021-06-18 00:43:20 +0300

    feat(issues): add routes dictionary

--- packages/issues/src/hooks/router.ts
+++ packages/issues/src/hooks/router.ts
@@ -1,13 +1,24 @@
 import { useRouter as NextRouter } from 'next/router';
 
+export const routes = {
+    index: () => '/',
+    createQueue: () => '/queues/new',
+    queue: (id: string) => `/queues/${id}`,
+    queueBoards: (id: string) => `/queues/${id}/boards`,
+    queueInsights: (id: string) => `/queues/${id}/insights`,
+    queueSettigns: (id: string) => `/queues/${id}/settings`,
+    issue: (id: string) => `/issues/${id}`,
+    createIssue: () => '/issues/new',
+};
+
 export const useRouter = () => {
     const router = NextRouter();
 
     return {
-        index: () => router.push('/'),
-        queue: (id: string) => router.push(`/queues/${id}`),
-        createQueue: () => router.push('/queues/new'),
-        issue: (id: string) => router.push(`/issues/${id}`),
-        createIssue: () => router.push('/issues/new'),
+        index: () => router.push(routes.index()),
+        queue: (id: string) => router.push(routes.queue(id)),
+        createQueue: () => router.push(routes.createQueue()),
+        issue: (id: string) => router.push(routes.issue(id)),
+        createIssue: () => router.push(routes.createIssue()),
     };
 };
